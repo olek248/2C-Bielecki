94a858b751493910df1a045c09d11b008b303b3e
Mlodziutki <joemamma@kys.com>
2023-10-03 09:55:20 +0200

    dni

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -3,10 +3,22 @@
 
 #include <iostream>
 
-int main()
-{
+/*
+*. Program obliczaj¹cy pole prostok¹ta.
+*. Program obliczaj¹cy objêtoœæ sto¿ka.
+*. Program obliczaj¹cy pole ko³a.
+*. Program obliczaj¹cy wartoœæ wyra¿enia a^2 + b^2
+*. Program obliczaj¹cy pole trójk¹ta o podstawie b i wysokoœci h
+*. Program obliczaj¹cy objêtoœæ kuli o promieniu r
+*. Program obliczaj¹cy pole trapezu o podstawach a i b oraz wysokoœci h
+*. Program obliczaj¹cy œredni¹ wa¿on¹ trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+*. Stwórz program, który pobiera od u¿ytkownika d³ugoœæ w metrach i przelicza j¹ na centymetry i milimetry, wyœwietlaj¹c wynik.
+*. Napisz program, który konwertuje wprowadzon¹ kwotê w jednej walucie na inn¹, korzystaj¹c z okreœlonego kursu wymiany.
+*/
+
        //Napisz program który wczyta liczbê od u¿ytkownika i j¹ wyœwietli na konsoli
-       
+void task1()
+{
     std::cout << "Podaj Liczbe\n";
     int numberFromUser;
     std::cin >> numberFromUser;
@@ -14,6 +26,71 @@ int main()
     std::cout << "Podales " << numberFromUser << "\n";
 }
 
+
+//program obliczaj¹cy œredni¹ artmetyczn¹ dwóch liczb
+void task2()
+{
+    int firstNumber, secondNumber;
+    std::cout << "Podaj pierwsz¹ liczbê\n";
+    std::cin >> firstNumber;
+    std::cout << "Podaj drug¹ liczbê\n";
+    std::cin >> secondNumber;
+
+    int average;
+   average = (firstNumber + secondNumber) / 2;
+
+   std::cout << "Œrednia to: " << average << "\n";
+}
+//program obiczaj¹cy pole prostok¹ta
+void task3()
+{
+    int firstSide, secondSide;
+    std::cout << "Podaj pierwszy bok prostok¹ta\n";
+    std::cin >> firstSide;
+    std::cout << "Podaj drugi bok prostok¹ta\n";
+    std::cin >> secondSide;
+
+    int area;
+    area = firstSide * secondSide;
+    std::cout << " Pole wynosi ";
+    std::cout << area;
+}
+
+//program obliczaj¹cy objêtoœæ sto¿ka
+void task4()
+{
+    int h, r, V;
+    std::cout << "Podaj wysokoœæ sto¿ka\n";
+    std::cin >> h;
+    std::cout << "Podaj promieñ sto¿ka\n";
+    std::cin >> r;
+
+    int area;
+    area = 1 / 3 * h * 3, 14 * r * r;
+    std::cout << "Objêtoœæ sto¿ka wynosi ";
+    std::cout << area;
+
+
+
+
+
+
+}
+
+
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    //task1();
+    //task2();
+    //task3();
+    task4();
+      
+
+
+}
+  
 /*
 // Algorytm- skoñczony zbiór instrukcji, który rozwi¹zuje zadany problem. Okreœla te¿ kolejnoœæ wykonywanych instrukcji. 
 zapis algorytmu:
