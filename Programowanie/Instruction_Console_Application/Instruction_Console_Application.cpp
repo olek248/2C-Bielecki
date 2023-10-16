ad2502aed4a0417a9901333f3f26edbf16dfad3c
Mlodziutki <joemamma@kys.com>
2023-10-16 16:01:55 +0200

    zielone zielone robie sobie zielone

--- Programowanie/Instruction_Console_Application/Instruction_Console_Application.cpp
+++ Programowanie/Instruction_Console_Application/Instruction_Console_Application.cpp
@@ -30,12 +30,35 @@ void task1()
 		std::cout << "liczba jest równa zero\n";
 	
 	std::cout << "kolejna instrukcja\n";
+}
+
+//napisz program, który wyœwietli czy liczba jest parzysta czy nie parzysta
+void task2()
+{
+	int number;
+	std::cout << "podaj liczbe\n";
+	std::cin >> number;
+	
+	int rest = number % 2;
+	if (rest == 0)
+		std::cout << "liczba jest parzysta\n";
+	if (rest != 0) // rest == 1
+		std::cout << "liczba jest nieparzysta\n";
 
 }
 
+
+
+
+
+
+
+
+
 int main()
 {
-	task1();
+	//task1();
+	task2();
 
 }
 
