299bcfedf87c79d7e467069aa8044de76ae9db6b
Mlodziutki <joemamma@kys.com>
2023-11-14 09:44:26 +0100

    tak

--- Programowanie/LoopDoWhileConsole_Apllication/LoopDoWhileConsole_Apllication.cpp
+++ Programowanie/LoopDoWhileConsole_Apllication/LoopDoWhileConsole_Apllication.cpp
@@ -50,12 +50,99 @@ void task2()
 
 	std::cout << "liczba pobrana od u¿ytkownika " << numberFromUser << "\n";
 }
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbê\n";
+	std::cin >> number;
 
+	int sum = 0;
+	int rest;
+
+	/*
+	rest = number % 10;
+	sum = sum + rest;
+	number = number / 10;
+	if (number != 0)
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+		if (number != 0)
+		{
+			rest = number % 10;
+			sum = sum + rest;
+			number = number / 10;
+			if (number != 0)
+			{
+				rest = number % 10;
+				sum = sum + rest;
+				number = number / 10;
+				//if (number != 0) ...
+			}
+		}
+	}
+	*/
+
+	do
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+	} while (number != 0);
+
+	std::cout << "Suma " << sum << "\n";
+	//4125
+}
+//poproœ u¿ytkownika o podawanie liczb, a¿ wprowadzi zero.
+//oblicz sumê oraz œredni¹ artmetyczn¹ wprowadzonych liczb.
+void task4()
+{
+	int number;
+	int sum = 0;
+	int numberOfNumbers = 0;
+	std::cout << "podaj liczbê\n";
+	std::cin >> number;
+	sum = sum + number;
+	if (number != 0)
+	{
+		std::cout << "podaj liczbê\n";
+		std::cin >> number;
+		sum = sum + number;
+		numberOfNumbers++;
+	}
+	if (number != 0)
+	{
+		std::cout << "podaj liczbê\n";
+		std::cin >> number;
+		sum = sum + number;
+		numberOfNumbers++;
+	}
+	if (number != 0)
+	{
+		std::cout << "podaj liczbê\n";
+		std::cin >> number;
+		sum = sum + number;
+		numberOfNumbers++;
+	}
+	do
+	{
+		std::cout << "podaj liczbê\n";
+		std::cin >> number;
+		sum = sum + number;
+		numberOfNumbers++;
+	} while (number != 0);
+	std::cout << "suma wszystkich liczb to " << sum << "\n";
+	double avg = sum / numberOfNumbers;
+
+}
 
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	//task3();
+	task4();
 }
 
 
