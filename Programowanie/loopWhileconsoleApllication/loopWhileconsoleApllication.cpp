ee904826442e72eacfd7da58075487b574e7e146
Mlodziutki <joemamma@kys.com>
2023-11-21 09:00:36 +0100

    tak

---
+++ Programowanie/loopWhileconsoleApllication/loopWhileconsoleApllication.cpp
@@ -0,0 +1,19 @@
+// loopWhileconsoleApllication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+void task1()
+{
+	int firstNumber, secondNumber;
+	std::cout << "podaj pierwsz¹ liczbê\n";
+	std::cin >> firstNumber;
+	std::cout << "podaj drug¹ liczbê\n";
+	std::cin >> secondNumber;
+
+	int nwd;
+	nwd =
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	task1();
+}
