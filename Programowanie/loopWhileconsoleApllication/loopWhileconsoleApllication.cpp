2c48e8dea09e313a202ba8e301276eaa53830d88
Mlodziutki <joemamma@kys.com>
2023-11-28 09:34:55 +0100

    kawazaki commit

--- Programowanie/loopWhileconsoleApllication/loopWhileconsoleApllication.cpp
+++ Programowanie/loopWhileconsoleApllication/loopWhileconsoleApllication.cpp
@@ -19,9 +19,49 @@ void task1()
 
 	std::cout << "NWD jest równe " << nwd << "\n";
 }
+void task2()
+{
+	int firstNumber, secondNumber, NWD, devinded;
+	std::cout << "podaj pierwsz¹ liczbê\n";
+	std::cin >> firstNumber;
+	std::cout << "podaj drug¹ liczbê\n";
+	std::cin >> secondNumber;
+	NWD = 1;
+	devinded = 2;
+	int tmpfirstNumber, tmpsecondNumber = secondNumber;
+	while (tmpfirstNumber >= devinded
+		&& tmpsecondNumber % devinded == 0)
+	{
+		if (tmpfirstNumber % devinded == 0
+			&& tmpsecondNumber % devinded == 0)
+		{
+			tmpfirstNumber = tmpfirstNumber / devinded;
+			tmpsecondNumber /= devinded;
+			NWD *= devinded;
+		}
+		else
+			devinded++;
+	}
+	std::cout << "NWD = " << NWD << "\n";
+
+
+
+
+	int a = firstNumber, b = secondNumber;
+	while (b != 0)
+	{
+		int tmpA = a;
+		a = b;
+		b = tmpA % b;
+	}
+	NWD = a;
+	std::cout << "NWD = " << NWD << "\n";
+}
 	int main()
 	{
 		setlocale(LC_CTYPE, "polish");
-		task1();
+		//task1();
+		task2();
+		
 	}
 
