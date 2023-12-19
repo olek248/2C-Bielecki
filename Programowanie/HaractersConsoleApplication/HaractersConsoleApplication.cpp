d18ef0699e7eabbe8fe4424d6d827432b6b45ec3
Mlodziutki <joemamma@kys.com>
2023-12-19 09:51:00 +0100

    lekcja

--- Programowanie/HaractersConsoleApplication/HaractersConsoleApplication.cpp
+++ Programowanie/HaractersConsoleApplication/HaractersConsoleApplication.cpp
@@ -8,6 +8,22 @@ typy znakowe:
 *   char - 1 bajt ca³kowitoliczbowa ze znakiem <-128, 128
 */
 //napisz program który pobierze znak od u¿ytkownika i wyœwietli go.
+
+/*
+* Napisz program, który bêdzie prosi³ o has³o. Nie przepuœci dalej dopóki nie zostanie ono podane prawid³owo.
+* Napisz program, który pobiera od u¿ytkownika ci¹g znaków i wyœwietla liczbê samog³osek i spó³g³osek w tym ci¹gu.
+* Poproœ u¿ytkownika o wprowadzenie liczby ca³kowitej w systemie dziesiêtnym. Nastêpnie skonwertuj tê liczbê na system dwójkowy (binarny) i wyœwietl wynik.
+* Program sprawdzaj¹cy czy podany ci¹g znaków jest palindromem (czyli takim, który czytany od ty³u jest taki sam, jak czytany od przodu, np. "kajak")
+* Program sprawdzaj¹cy czy podane dwa s³owa s¹ anagramami (czyli takimi, które zawieraj¹ te same litery, ale w innym uk³adzie, np. "klasa" i "salka")
+* Program wyci¹gaj¹cy informacje z numeru PESEL
+* Program implementuj¹cy algorytm szyfrowania Cezara (proste szyfrowanie, w którym ka¿dy znak w tekœcie jest zastêpowany innym znakiem, przesuniêtym o sta³¹ liczbê pozycji w alfabecie).
+
+* Program który na wejœciu przyjmie równanie a na wyjœciu da równanie w odwrotnej notacji polskiej ONP. Np. na wejœciu 2+3*4 na wyjœcu da 234*+
+* Program, który na wejœciu przyjmie rówanie w ONP a na wyjœciu wyœwietli wynik rówania.
+* Algorytm szyfrowania AtBash Cipher - algorytm szyfruj¹cy, w którym ka¿da litera jest zamieniana na literê z przeciwnej strony alfabetu (np. A na Z, B na Y itd.)
+*/
+
+//Napisz program który pobierze znak od u¿ytkownika i wyœwieli go.
 void task1()
 {
     char characterfromUser;
@@ -27,10 +43,84 @@ void task2()
     else
         std::cout << "nie jest to ma³a litera alfabetu\n";
 }
+
+//napisz program, który poprosi ciê o twoje imiê i ciê przywita.
+void task3()
+{
+    std::string userName;
+    std::cout << "podaj swojê imiê\n";
+    std::cin >> userName;
+    
+    std::cout << "Witaj " << userName << " tutaj.\n";
+}
+
+//program sprawdzaj¹cy czy podane has³o jest poprawne
+//(np. jeœli has³o jest "abc123" program powinien wyœwietliæ "has³o poprawne"
+//jeœli inne, powinien wyœwietliæ "has³o niepoprawne").
+void task4()
+{
+    std::string password;
+    std::cout << "podaj has³o: \n";
+    std::cin >> password;
+    if (password == "abc123")
+        std::cout << "has³o jest poprawne\n";
+    else
+        std::cout << "has³o niepoprawne\n";
+}
+
+//napisz program, który wczyta ³añcuch znaków i policzy ile jest ma³ych liter 'a'.
+void task5()
+{
+    std::string textFromUser;
+    std::cout << "podaj ³añcuch znaków\n";
+    std::cin >> textFromUser;
+
+    int numberOfCharacters = 0;
+    
+    std::cout << "Pierwszy znak to " << textFromUser[0] << "\n";
+    textFromUser[1] = 'x';
+    std::cout << "Iloœæ znaków " << textFromUser.length() << "\n";
+
+    for (int i = 0; i < textFromUser.length(); i++)
+    {
+        if (textFromUser[1] == 'a')
+            numberOfCharacters++;
+    }
+
+}
+
+//* Napisz program, który bêdzie prosi³ o has³o. 
+// Nie przepuœci dalej dopóki nie zostanie ono podane prawid³owo.
+void task6()
+{
+    std::string password;
+    
+    do
+    {
+        std::cout << "podaj has³o\n";
+        std::cin >> password;
+    } while (password != "majonezkielecki");
+    std::cout << "gratulacje u¿ytkowniku poprawne has³o";
+}
+
+//* Napisz program, 
+//który pobiera od u¿ytkownika ci¹g znaków i wyœwietla liczbê samog³osek i spó³g³osek w tym ci¹gu.
+void task7()
+{
+    std::string userText;
+    std::cout << "podaj ci¹g znaków\n";
+    std::cin >> userText;
+}
 int main()
 {
+    setlocale(LC_CTYPE, "polish");
     //task1();
-    task2();
+    //task2();
+    //task3();
+    //task4();
+    //task5();
+    //task6();
+    task7();
 }
 
 
