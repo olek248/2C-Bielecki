f75adc3bb1e77ee8285559111d8fe265b3d88ba3
y <kgf@f.pl>
2024-09-19 09:30:55 +0200

    struktury

---
+++ Programowanie/FirstStruct.Application/FirstStruct.Application.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct point
+{
+	double x;
+	double y;
+};
+
+void getPointValue(double& x, double& y)
+{
+	std::cout << "Podaj x\n";
+	std::cin >> x;
+	std::cout << "Podaj y\n";
+	std::cin >> y;
+}
+
+//napisz program, który oblicza odleg³oœæ punktu 2D od innego punktu
+void task1()
+{
+	//double x, y;
+	point firstPoint;
+	//firstPoint.x = 5.9;
+
+	getPointValue(firstPoint.x, firstPoint.y);
+
+	double distance = sqrt(firstPoint.x * firstPoint.x + firstPoint.y * firstPoint.y);
+
+	std::cout << "odleg³oœæ to " << distance << "\n";
+}
+
+
