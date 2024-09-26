3e58bcbbfb16347cdd736518e9616cef6aec29c4
q <q>
2024-09-26 09:53:40 +0200

    brrrr

--- Programowanie/FirstStruct.Application/FirstStruct.Application.cpp
+++ Programowanie/FirstStruct.Application/FirstStruct.Application.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
+#include  "Tasks.h"
 
-struct point
-{
-	double x;
-	double y;
-};
 
-void getPointValue(double& x, double& y)
+int main()
 {
-	std::cout << "Podaj x\n";
-	std::cin >> x;
-	std::cout << "Podaj g\n";
-	std::cin >> y;
+	setlocale(LC_CTYPE, "polish");
+	task4();
 }
-
-//napisz program, który oblicza odleg³oœæ punktu 2D od innego punktu
-void task1()
-{
-	//double x, y;
-	point firstPoint;
-	//firstPoint.x = 5.9;
-
-	getPointValue(firstPoint.x, firstPoint.y);
-
-	double distance = sqrt(firstPoint.x * firstPoint.x + firstPoint.y * firstPoint.y);
-
-	std::cout << "odleg³oœæ to " << distance << "\n";
-}
-
 
---
+++ Programowanie/FirstStruct.Application/Task4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+struct bankAccount
+{
+	double balance;
+	std::string owner;
+	std::string currency;
+};
+
+void accountInformation(bankAccount &account)
+{
+	std::cout << "Informacja o koncie bankowym\n";
+	std::cout << "W³aœciciel: " << account.owner << "\n";
+	std::cout << "Soldo: " << account.balance << " " << account.currency << "\n";
+}
+
+void depositToAccount(bankAccount& account, double amount)
+{
+	if (amount >= 0)
+		account.balance = account.balance + amount;
+}
+
+
+void task4()
+{
+	bankAccount firstAccount;
+	firstAccount.balance = 7200;
+	firstAccount.owner = "Jan Kowalski";
+	firstAccount.currency = "z³";
+
+	accountInformation(firstAccount);
+
+	bankAccount secondAccount;
+	secondAccount.balance = 3200;
+	secondAccount.owner = "Ewa Nowak";
+	secondAccount.currency = "z³";
+
+	accountInformation(secondAccount);
+}
