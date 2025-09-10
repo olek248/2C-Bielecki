296549919788034430a884c854d85946089d771c
Sala213 <Sala213@ezse.pl>
2025-09-10 15:57:37 +0200

    koniec drugiej lekcji

--- Programowanie/First.ConsoleApp/Program.cs
+++ Programowanie/First.ConsoleApp/Program.cs
@@ -5,3 +5,27 @@ Console.WriteLine("Hello word!");
 string name = "Jan";
 string surname = "Kowalski";
 Console.WriteLine("Witaj " + name + surname + " tutaj!!!!");
+Console.WriteLine("Witaj {0} {1} tutaj!!!!!", name, surname);
+Console.WriteLine($"Witaj {name} {surname} tutaj!!!!");
+
+Console.WriteLine("Prędkość to km\\h");
+Console.WriteLine(@"Prędkość to km\h");
+
+int firstNumber = 5;
+int secondNumber = firstNumber;
+secondNumber++;
+Console.WriteLine($"Pierwsza liczba to: {firstNumber}");
+Console.WriteLine($"Druga liczbta to: {secondNumber}");
+
+void ParametrTest_v1(int p)
+{
+    Console.WriteLine($"Parametr w ParametrTest_v1 {p}");
+    p++;
+    Console.WriteLine($"Parametr w ParametrTest_v1 {p}");
+}
+
+firstNumber = 15;
+Console.WriteLine($"firstNumber przed {firstNumber}");
+ParametrTest_v1(firstNumber);
+Console.WriteLine($"firstNumber po {firstNumber}");
+//ParametrTest_v1(99);
