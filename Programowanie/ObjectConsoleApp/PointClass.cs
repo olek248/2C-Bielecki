ef84d2bc7e32261d5f641e8ae1b7dff3aa1992df
Sala213 <Sala213@ezse.pl>
2025-09-17 15:57:18 +0200

    dwa

---
+++ Programowanie/ObjectConsoleApp/PointClass.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectConsoleApp
+{
+    internal class PointClass
+    {
+        public int x;
+        public int y;
+    }
+}
---
+++ Programowanie/ObjectConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using ObjectConsoleApp;
+
+PointStruct firstPointStruct;
+firstPointStruct.x = 5;
+firstPointStruct.y = 6;
+
+Console.WriteLine($"firstPointStruct ({firstPointStruct.x} , {firstPointStruct.y})");
+
+PointStruct secondPointStruct = firstPointStruct;
+secondPointStruct.x = 7;
+Console.WriteLine($"firstPointStruct ({firstPointStruct.x} , {firstPointStruct.y})");
+Console.WriteLine($"secondPointStruct ({secondPointStruct.x} , {secondPointStruct.y})");
+
+void ParametrTestStruct_v1(PointStruct ps)
+{
+    Console.WriteLine($"pParametrTestStruct_v1 ({ps.x} , {ps.y})");
+    ps.x = 99;
+    Console.WriteLine($"pParametrTestStruct_v1 ({ps.x} , {ps.y})");
+}
+
+void ParametrTestStruct_v2(PointStruct ps)
+{
+    Console.WriteLine($"pParametrTestStruct_v2 ({ps.x} , {ps.y})");
+    ps.x = 99;
+    Console.WriteLine($"pParametrTestStruct_v2 ({ps.x} , {ps.y})");
+}
+
+ParametrTestStruct_v1(firstPointStruct);
+Console.WriteLine($"firstPointStruct ({firstPointStruct.x} , {firstPointStruct.y})");
+
+ParametrTestStruct_v2(firstPointStruct);
+Console.WriteLine($"firstPointStruct ({firstPointStruct.x} , {firstPointStruct.y})");
+
+//--------------------------------------------------------------------------------------------------
+
+PointClass firstPointClass = new PointClass();
+firstPointClass.x = 15;
+firstPointClass.y = 99;
+
+Console.WriteLine($"firstPointClass ({firstPointClass.x} , {firstPointClass.y})");
+
+PointClass secondPointClass = firstPointClass;
+secondPointClass.x = 29;
+Console.WriteLine($"firstPointClass ({firstPointClass.x} , {firstPointClass.y})");
+Console.WriteLine($"secondPointClass ({secondPointClass.x} , {secondPointClass.y})");
