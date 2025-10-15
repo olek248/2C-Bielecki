93ac312ca796d8e7ce0ce8722d2f66124b6a7227
Sala213 <Sala213@ezse.pl>
2025-10-15 15:58:27 +0200

    koniec

---
+++ Programowanie/PracticalTaskConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using PracticalTaskConsoleApp;
+using PracticalTaskConsoleApp.Styczen2023_1;
+
+Task1 task1 = new Task1();
+task1.task();
---
+++ Programowanie/PracticalTaskConsoleApp/Styczen2023_1/Task1.cs
@@ -0,0 +1,29 @@
+﻿namespace PracticalTaskConsoleApp.Styczen2023_1
+{
+    internal class Task1
+    {
+        public void task()
+        {
+            Console.WriteLine("Podaj liczbę a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbę b: ");
+            int b = int.Parse(Console.ReadLine());
+            int nwd(int a,  int b)
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                    {
+                        a = a - b;
+                    }
+                    else
+                    {
+                        b = b - a;
+                    }
+                }
+            }
+            Console.WriteLine($"NWD = {a}");
+        }
+    }
+}
