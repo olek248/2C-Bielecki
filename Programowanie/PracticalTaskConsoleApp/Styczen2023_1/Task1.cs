def9bad10ed3adeeec7f2b11426c0629510fe22a
Sala213 <Sala213@ezse.pl>
2025-10-16 16:50:34 +0200

    koniec

--- Programowanie/PracticalTaskConsoleApp/Styczen2023_1/Task1.cs
+++ Programowanie/PracticalTaskConsoleApp/Styczen2023_1/Task1.cs
@@ -4,26 +4,43 @@
     {
         public void task()
         {
+            static int Nwd(int a, int b)
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a = a - b;
+                    else
+                        b = b - a;
+                }
+                return a;
+            }
+
             Console.WriteLine("Podaj liczbę a: ");
             int a = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Podaj liczbę b: ");
             int b = int.Parse(Console.ReadLine());
-            int nwd(int a,  int b)
+
+            if(a <= 0 || b <= 0)
+                Console.WriteLine("podane liczby nie są dodatnie!!!");
+            else
             {
-                while (a != b)
-                {
-                    if (a > b)
-                    {
-                        a = a - b;
-                    }
-                    else
-                    {
-                        b = b - a;
-                    }
-                }
+                int wynik = Nwd(a, b);
+                Console.WriteLine($"Największy wspólny dzielnik to: {wynik}");
+
             }
-            Console.WriteLine($"NWD = {a}");
+        
+            /*
+             *********************************************************************
+             nazwa funkcji: Nwd
+            opis funkcji: funkcja obliczna najwiekszy wspolny dzielnik liczby a i b
+            parametry: a - paremetr przechowujacy wartosc pierwszej liczby
+                       b - parametr przechowujacy wartosc drugiej liczby
+            zwracany typ i opis: typ zwracany to int i zwracany jest najwiekszy wspolny dzielnik obu liczb
+            ***************************************************************************
+             */
+         
         }
     }
 }
