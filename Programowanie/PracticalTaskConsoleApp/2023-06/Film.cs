26e4b5180578ccdc79b918d20e02ecd33ff98164
Sala213 <Sala213@ezse.pl>
2025-10-29 15:52:03 +0100

    tak

---
+++ Programowanie/PracticalTaskConsoleApp/2023-06/Film.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTaskConsoleApp._2023_06
+{
+
+    internal class movie
+    {
+
+
+        private string tytul;
+        private int liczbaWypozyczen;
+
+        public void ustawTytul(string t)
+        {
+            if (t.Length <= 20) tytul = t;
+            else Console.WriteLine("Tytuł za długi!");
+        }
+
+        public string PobierzTytul() => tytul;
+        public int PobierzLiczbeWypozyczen() => liczbaWypozyczen;
+
+        public void Inkrementuj() => liczbaWypozyczen++;
+
+
+
+        static void brrrr()
+        {
+            movie film = new movie();
+            film.ustawTytul("Matrix");
+
+            Console.WriteLine("Przed wypożyczeniem:");
+            Console.WriteLine($"{film.PobierzTytul()} - {film.PobierzLiczbeWypozyczen()} wypożyczeń");
+
+            film.Inkrementuj();
+
+            Console.WriteLine("\n po wypożyczeniu:");
+            Console.WriteLine($"{film.PobierzTytul()} - {film.PobierzLiczbeWypozyczen()} wypożyczeń");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
--- Programowanie/PracticalTaskConsoleApp/Program.cs
+++ Programowanie/PracticalTaskConsoleApp/Program.cs
@@ -1,5 +1,10 @@
 ﻿using PracticalTaskConsoleApp;
+using PracticalTaskConsoleApp._2023_06;
 using PracticalTaskConsoleApp.Styczen2023_1;
 
 Task1 task1 = new Task1();
+movie Film = new movie();
 task1.task();
+movie.
+
+
