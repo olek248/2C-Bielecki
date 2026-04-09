79978a2f1cfa9ccc5afed44a5852a80f675eef68
Michal Opalinski <michalopalinski@o2.pl>
2026-04-09 12:03:16 +0200

    dokończenie zadań Task1

--- Programowanie/LinqPracticalTasksConsoleApp/Task1.cs
+++ Programowanie/LinqPracticalTasksConsoleApp/Task1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -88,7 +88,7 @@ namespace LinqPracticalTasksConsoleApp
         };
 
             // === POZIOM 1 ===
-
+            /*
             // Zadanie 1
             var q1 = people.Where(p => p.City == "Kraków");
             Print("Zadanie 1", q1);
@@ -104,10 +104,112 @@ namespace LinqPracticalTasksConsoleApp
             // Zadanie 4
             var q4 = people.OrderByDescending(p => p.LastName);
             Print("Zadanie 4", q4);
-
+            
             // Zadanie 5
-             
+            var q5 = people.Select(p => new { p.FirstName, p.LastName });
+            Print("Zadanie 5", q5);
+            
+            //Zadanie 6
+            var q6 = people.Select(p => p.City).Distinct();
+            Print("Zadanie 6", q6);
+            
+            //Zadanie 7
+            var q7 = people.Count(p => p.City == "Warszawa").ToString();
+            Print("Zadanie 7: Liczba osob z warszawy:", q7);
+            
+            //Zadanie 8
+            var q8 = people.Average(p => p.Salary);
+            Console.WriteLine(q8);
+            
+            //Zadanie 9
+            var q9 = people.OrderBy(p => p.Age).FirstOrDefault();
+            Console.WriteLine(q9);
+            
+            //Zadanie 10
+            bool czyJestKtosZGdanska = people.Any(p => p.City == "Gdańsk");
 
+            if(czyJestKtosZGdanska)
+            {
+                Console.WriteLine("Zadanie 10: Tak, na liście jest przynajmniej jedna osoba z Gdańska");
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 10: Nie, nikt na liście nie mieszka w Gdańsku");
+            }
+            
+            //Zadanie 11
+            var q11 = people.OrderBy(p => p.City).ThenByDescending(p => p.Salary);
+            Print("Zadanie11", q11);
+            
+            //Zadanie 12
+            var q12 = people.Where(p => p.Age >= 25 && p.Age <= 35);
+            Print("Zadanie 12:", q12);
+            
+            //Zadanie 13
+            var q13 = people.Sum(p => p.City == "Kielce" ? p.Salary : 0);
+            Console.WriteLine($"Zadanie 13 {q13}");
+            
+            //Zadanie 14
+            var q14 = people.Where(p => p.Salary > 10000).First();
+            Console.WriteLine($"Zadanie 14 {q14}");
+
+            //Zadanie 15
+            var q15 = people.OrderBy(p => p.LastName).LastOrDefault();
+            Console.WriteLine($"Zadanie 15 {q15}");
+
+            //Zadanie 16
+            var q16 = people.Select(p => new { FirstAndSecondName = $"{p.FirstName} {p.LastName}", Town = "(" + p.City + " )" });
+            Print("Zadanie 16", q16);
+
+            //Zadanie 17
+            var q17 = people.All(p => p.Age >= 18);
+            Console.WriteLine($"Zadanie 17 {q17}");
+
+            //Zadanie 18
+            var q18 = people.Count(p => p.Gender == Gender.Female);
+            Console.WriteLine($"Zadanie 18 {q18}");
+            
+            //Zadanie 19
+            var q19 = people.Where(p => p.Salary > people.Average(p => p.Salary));
+            Print("Zadanie 19", q19);
+
+            //Zadanie 20
+            var q20 = people.Where(p => p.City == "Kraków").OrderByDescending(p => p.Age).First();
+            Console.WriteLine("Zadanie 20", q20);
+
+            //Zadanie 22
+            var q22 = people.Where(p => p.Skills.Contains("C#"));
+            Print("Zadanie 22", q22);
+            
+            //Zadanie 23
+            var q23 = people.Where(p => p.Skills.Count() >= 3);
+            Print("Zadanie 23", q23);
+
+            //Zadanie 24
+            var q24 = people.Where(p => p.City == "Warszawa").OrderByDescending(p => p.Age).Select(p => $"{p.FirstName} {p.LastName}, {p.Age} lat, {p.Salary} zł");
+            Print("Zadanie 24", q24);
+
+            //Zadanie 25
+            var q25 = people.Count(p => p.Skills.Contains("Azure")) > 0;
+            Console.WriteLine($"Zadanie 25 {q25.ToString()}");
+
+            //Zadanie 26
+            var q26 = people.All(p => p.Salary >= 4000);
+            Console.WriteLine($"Zadanie 26 {q26.ToString()}");
+
+            //Zadanie 27
+            var q27 = people.OrderByDescending(p => p.Salary).First();
+            Console.WriteLine($"Zadanie 27 {q27.ToString()}");
+
+            //Zadanie 28
+            var q28 = people.OrderBy(p => p.Salary).First();
+            Console.WriteLine($"Zadanie 28 {q28}");
+
+            //Zadanie 29
+            var q29 = people.Where(p => p.Age == people.Max(pp => pp.Age));
+            Print("Zadanie 29", q29);
+            */
+          
         }
     }
 }
